853e1943c64bfac7396a1045a0891e7ca0e9e63d
Sujan4321das <dsujan180@gmail.com>
2021-12-18 00:30:55 +0530

    3rd Commit

--- sts/urls.py
+++ sts/urls.py
@@ -23,7 +23,7 @@ from django.views.static import serve
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('',include('appsite.urls')),
     path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
